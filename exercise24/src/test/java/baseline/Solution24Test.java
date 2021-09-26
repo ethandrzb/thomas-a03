fba66282e73c568de9970115c3ca68423244f24a
ethandrzb <ethandrzb@gmail.com>
2021-09-26 18:56:22 -0400

    Added tests for isAnagram

---
+++ exercise24/src/test/java/baseline/Solution24Test.java
@@ -0,0 +1,32 @@
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution24Test
+{
+    @Test
+    void isAnagram_Case_1()
+    {
+        Solution24 sol = new Solution24();
+
+        assertTrue(sol.isAnagram("note", "tone"));
+    }
+
+    @Test
+    void isAnagram_Case_2()
+    {
+        Solution24 sol = new Solution24();
+
+        assertFalse(sol.isAnagram("abc", "cde"));
+    }
+
+    @Test
+    void isAnagram_Case_3()
+    {
+        Solution24 sol = new Solution24();
+
+        assertFalse(sol.isAnagram("abcd", "abc"));
+    }
+}
