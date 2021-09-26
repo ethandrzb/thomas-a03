55551b79760fd67e3492b4613b3abdb767bdbdc4
ethandrzb <ethandrzb@gmail.com>
2021-09-26 18:41:06 -0400

    Start of Solution24

---
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -0,0 +1,38 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution 24
+ *  Copyright 2021 Ethan Thomas
+ */
+
+package baseline;
+
+public class Solution24
+{
+    public static void main(String[] args)
+    {
+        // Print program instructions
+
+        // Prompt for first string
+        // Store input as string
+
+        // Prompt for second string
+        // Store input as string
+
+        // Determine whether the two strings are anagrams
+
+        // Print appropriate response
+
+        // Exit
+    }
+    private boolean isAnagram(String str1, String str2)
+    {
+        // Check lengths of str1 and str2. If they don't match, they can't be anagrams.
+
+        // Compare the characters in each string, going forward for str1 and backward for str2
+
+        // Return false if a mismatch occurs at any point
+
+        // Otherwise, return true
+
+        return true;
+    }
+}
