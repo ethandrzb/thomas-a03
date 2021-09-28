8408e4542cba72df6508efdbdcae095273d738a4
ethandrzb <ethandrzb@gmail.com>
2021-09-27 22:24:30 -0400

    Drafted regexes for passwordValidator helper functions

--- exercise25/src/main/java/baseline/Solution25.java
+++ exercise25/src/main/java/baseline/Solution25.java
@@ -37,20 +37,45 @@ public class Solution25
     private boolean isVeryWeak(String password)
     {
         // Check if password only contains numbers (very weak)
+
+        // Regex = "/^\d+$/g"
     }
 
     private boolean isWeak(String password)
     {
         // Check if password only contains letters (weak)
+
+        // Regex = "/^[a-zA-Z\D]+$/g"
     }
 
     private boolean isString(String password)
     {
         // Check if password contains letters, and at least one number (strong)
+
+        // Match only alphanumeric passwords
+        // Regex = "/^[a-zA-Z\d]+$/g"
+
+        // Make sure password has at least one digit
+        // Regex = "/\d+/g"
     }
 
     private boolean isVeryStrong(String password)
     {
         // Check if password contains letters, numbers, and special characters (very strong)
+
+        // This approach starts by checking that a password matches at least one of 3 cases (first regex)
+        // Afterwards, each case is checked individually (remaining regexes)
+
+        // Match passwords with alphanumeric and special characters
+        // Regex = "/[a-zA-Z\d !"#$%&'()*+,\-./:;<=>?@[\]^_`{|}~]+/g"
+
+        // Make sure password has letters
+        // Regex = "/[a-zA-Z]+/g"
+
+        // Make sure password has numbers
+        // Regex = "/\d+/g"
+
+        // Make sure password has special characters
+        // Regex = "/[ !"#$%&'()*+,\-./:;<=>?@[\]^_`{|}~]+/g"
     }
 }
