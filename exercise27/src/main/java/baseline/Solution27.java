294a1582b8ba288d95b529a822b1195db9059bb8
ethandrzb <ethandrzb@gmail.com>
2021-09-29 21:24:32 -0400

    Start of Solution27

---
+++ exercise27/src/main/java/baseline/Solution27.java
@@ -0,0 +1,63 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution 27
+ *  Copyright 2021 Ethan Thomas
+ */
+
+package baseline;
+
+public class Solution27
+{
+    public static void main(String[] args)
+    {
+        // Prompt user for first name
+
+        // Prompt user for last name
+
+        // Prompt user for ZIP code
+
+        // Prompt user for employee ID
+
+        // Display validation report
+
+        // Exit
+    }
+
+    public void printInputValidationReport(String firstName, String lastName, String zipCode, String employeeID)
+    {
+        // Validate firstName
+
+        // Validate lastName
+
+        // Validate zipCode
+
+        // Validate employeeID
+
+        // Generate report
+
+        // Print report
+    }
+
+    private boolean isValidFirstName(String firstName)
+    {
+        // Check if firstName is non-empty
+
+        // Check if firstName is at least two characters long
+    }
+
+    private boolean isValidLastName(String lastName)
+    {
+        // Check if lastName is non-empty
+
+        // Check if lastName is at least two characters long
+    }
+
+    private boolean isValidZIPCode(String zipCode)
+    {
+        // Check if zipCode is 5-digit number
+    }
+
+    private boolean isValidEmployeeID(String employeeID)
+    {
+        // Check if employeeID follows the specified format
+    }
+}
