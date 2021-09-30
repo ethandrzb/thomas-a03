16022a6e54f887a40e07263273b92643f2eed583
ethandrzb <ethandrzb@gmail.com>
2021-09-29 20:59:35 -0400

    Added unit tests

---
+++ exercise26/src/test/java/baseline/PaymentCalculatorTest.java
@@ -0,0 +1,26 @@
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PaymentCalculatorTest
+{
+    @Test
+    void PaymentCalculatorConstructor()
+    {
+        PaymentCalculator pcal = new PaymentCalculator(5000, 12, 100);
+
+        assertEquals(5000.0, pcal.getBalance(), 0.00001);
+        assertEquals(3.0 / 9125.0, pcal.getDailyRate(), 0.00001);
+        assertEquals(100.0, pcal.getMonthlyPayment(), 0.00001);
+    }
+
+    @Test
+    void calculateMonthsUntilPaidOff()
+    {
+        PaymentCalculator pcal = new PaymentCalculator(5000, 12, 100);
+
+        assertEquals(70, pcal.calculateMonthsUntilPaidOff());
+    }
+}
