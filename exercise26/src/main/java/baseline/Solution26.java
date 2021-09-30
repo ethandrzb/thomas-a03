a0b6d1a8f9dc53d845da1fc42d7833c81e50c625
ethandrzb <ethandrzb@gmail.com>
2021-09-29 21:00:21 -0400

    Finished PaymentCalculator

--- exercise26/src/main/java/baseline/Solution26.java
+++ exercise26/src/main/java/baseline/Solution26.java
@@ -24,31 +24,64 @@ public class Solution26
         // Compute number of months needed to pay off card
 
         // Display results
+
+        // Exit
+        System.exit(0);
     }
 }
 
 class PaymentCalculator
 {
     private final double balance;
-    private final double APR;
     private final double monthlyPayment;
 
     private final double dailyRate;
 
-    public PaymentCalculator(double balance, double APR, double monthlyPayment)
+    public PaymentCalculator(double balance, double apr, double monthlyPayment)
     {
         // Assign instance vars
+        this.balance = balance;
+        this.monthlyPayment = monthlyPayment;
+
+        // Convert apr from percentage to factor
+        // E.g. 5 ==> 0.05
+        apr /= 100;
 
         // Compute daily rate
+        dailyRate = apr / 365;
     }
 
     public int calculateMonthsUntilPaidOff()
     {
         // Compute number of months until card is paid off
+
+        return (int)Math.ceil(-(1.0 / 30.0) * (Math.log(1 + roundToNearestCent(balance / monthlyPayment) * (1 - Math.pow(1 + dailyRate, 30)))) / Math.log(1 + dailyRate));
     }
 
     private double roundToNearestCent(double amount)
     {
-        // Round to nearest penny
+        // Round up to next penny
+        amount *= 1000;
+        amount = Math.ceil(amount);
+        amount /= 10;
+        amount = Math.ceil(amount);
+        amount /= 100;
+
+        return amount;
+    }
+
+    public double getBalance()
+    {
+        return balance;
+    }
+
+    public double getMonthlyPayment()
+    {
+        return monthlyPayment;
+    }
+
+    public double getDailyRate()
+    {
+        return dailyRate;
     }
 }
