64f3584c530acff5a0f9cbc9a932a0e09089486c
ethandrzb <ethandrzb@gmail.com>
2021-10-01 13:18:33 -0400

    Baseline solution done.

--- exercise28/src/main/java/baseline/Solution28.java
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -5,21 +5,38 @@
 
 package baseline;
 
+import java.util.Scanner;
+
 public class Solution28
 {
+    private static final Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args)
     {
+        Solution28 sol = new Solution28();
+
         // Prompt user for 5 integers and compute their sum
+        int sum = sol.sum5Integers();
 
         // Display sum
+        System.out.println("The total is " + sum + ".");
 
         // Exit
+        System.exit(0);
     }
     public int sum5Integers()
     {
+        int runningTotal = 0;
+
         // Repeat 5 times
+        for(int i = 0; i < 5; i++)
+        {
             // Prompt user for integer
-            // Store integer
+            System.out.print("Enter a number: ");
             // Add this integer to running total
+            runningTotal += sc.nextInt();
+        }
+
+        return runningTotal;
     }
 }
