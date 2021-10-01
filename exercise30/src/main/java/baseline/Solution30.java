f5e80d1e821d5603a0859b66ff5c2ac05e6b256c
ethandrzb <ethandrzb@gmail.com>
2021-10-01 19:29:01 -0400

    Start of Solution30

---
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -0,0 +1,28 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution 30
+ *  Copyright 2021 Ethan Thomas
+ */
+
+package baseline;
+
+public class Solution30
+{
+    public static void main(String[] args)
+    {
+        // Generate multiplication table
+
+        // Display multiplication table
+
+        // Exit
+    }
+
+    private String generateMultiplicationTable()
+    {
+        // For each number i in [1, 12]
+            // Generate row of products
+            // For each number j in [1, 12]
+                // Append i * j to row
+            // Append row to table
+        // Return table
+    }
+}
