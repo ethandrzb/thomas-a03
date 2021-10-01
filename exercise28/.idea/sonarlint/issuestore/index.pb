c3f1d993d011458e4897a3aaa4b67d7938c1ca23
ethandrzb <ethandrzb@gmail.com>
2021-10-01 17:15:31 -0400

    Start of Solution29

--- exercise28/.idea/sonarlint/issuestore/index.pb
+++ exercise28/.idea/sonarlint/issuestore/index.pb
@@ -10,3 +10,9 @@ X
 :
 
 .gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+Z
+*src/test/java/baseline/Solution28Test.java,c\a\ca3be6eccbeb14d6fc5accdbe4b036a576e6f5bf
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+V
+&src/main/java/baseline/Solution28.java,2\d\2dbeeb7846b800614d9a4335dc03943178e3e266
---
+++ exercise29/src/main/java/baseline/Solution29.java
@@ -0,0 +1,30 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution 29
+ *  Copyright 2021 Ethan Thomas
+ */
+
+package baseline;
+
+public class Solution29
+{
+    public static void main(String[] args)
+    {
+        // Prompt user for rate of return
+
+        // Compute years needed to double investment
+
+        // Display years needed to double investment
+
+        // Exit
+    }
+
+    private double getDoubleFromUser(String prompt)
+    {
+        // Until the user enters valid input
+            // Display prompt
+
+            // Attempt to parse user input as double
+
+            // If successful, exit loop
+    }
+}
