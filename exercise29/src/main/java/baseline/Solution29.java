b4fbe134fb4a85519fef4cc1c2af948208c7f505
ethandrzb <ethandrzb@gmail.com>
2021-10-01 17:28:37 -0400

    Finished Solution29

--- exercise29/src/main/java/baseline/Solution29.java
+++ exercise29/src/main/java/baseline/Solution29.java
@@ -5,29 +5,69 @@
 
 package baseline;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Solution29
 {
+    private static final Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args)
     {
+        Solution29 sol = new Solution29();
+
         // Prompt user for rate of return
+        double rate = sol.getRateFromUser();
 
         // Compute years needed to double investment
+        double yearsNeeded = 72 / rate;
 
         // Display years needed to double investment
+        System.out.println("It will take " + yearsNeeded + " to double your initial investment.");
 
         // Exit
+        System.exit(0);
     }
 
-    private double getDoubleFromUser(String prompt)
+    private double getRateFromUser()
     {
+        double value;
+
         // Until the user enters valid input
+        while(true)
+        {
             // Display prompt
+            System.out.print("What is the rate of return? ");
 
             // Attempt to parse user input as double
+            try
+            {
+                value = sc.nextDouble();
 
-            // If successful, check if input is 0
-                // If so, display error message
-                // If not, exit loop
-            // If not, display error message
+                // If successful, check if input is 0
+                if(value == 0)
+                {
+                    throw new IllegalArgumentException();
+                }
+                else
+                {
+                    // If not, exit loop
+
+                    break;
+                }
+            }
+            catch(InputMismatchException e)
+            {
+                // If this fails, display error message
+                System.out.println("Input must be numeric.");
+
+                sc.nextLine();
+            }
+            catch(IllegalArgumentException e)
+            {
+                System.out.println("Input cannot be 0");
+            }
+        }
+        return value;
     }
 }
