b9138f073f91c527a6e96f2b2f03a600fcd925bc
ethandrzb <ethandrzb@gmail.com>
2021-10-01 13:13:58 -0400

    Start of Solution28

--- exercise27/.idea/sonarlint/issuestore/index.pb
+++ exercise27/.idea/sonarlint/issuestore/index.pb
@@ -7,3 +7,12 @@ X
 (gradle/wrapper/gradle-wrapper.properties,f\b\fbe448ebfc3eb2d4e308f6b8b043666f5b57235e
 ;
 gradlew.bat,2\a\2a45a911a8f1836b0b6c5b758962572012d8f8c3
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+Z
+*src/test/java/baseline/Solution27Test.java,8\9\89913e3799b326b70b15dc11b1d2d6d19e392826
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+V
+&src/main/java/baseline/Solution27.java,c\4\c4ea424aa0247fa241423fcc7495bda62cff644e
---
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -0,0 +1,25 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution 28
+ *  Copyright 2021 Ethan Thomas
+ */
+
+package baseline;
+
+public class Solution28
+{
+    public static void main(String[] args)
+    {
+        // Prompt user for 5 integers and compute their sum
+
+        // Display sum
+
+        // Exit
+    }
+    public int sum5Integers()
+    {
+        // Repeat 5 times
+            // Prompt user for integer
+            // Store integer
+            // Add this integer to running total
+    }
+}
