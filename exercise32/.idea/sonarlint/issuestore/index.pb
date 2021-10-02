b2ad6e0c43097b3eb74ea073b22edf426164089d
ethandrzb <ethandrzb@gmail.com>
2021-10-02 17:37:17 -0400

    Finished Solution33

--- exercise32/.idea/sonarlint/issuestore/index.pb
+++ exercise32/.idea/sonarlint/issuestore/index.pb
@@ -7,3 +7,12 @@ X
 gradlew.bat,2\a\2a45a911a8f1836b0b6c5b758962572012d8f8c3
 ?
 settings.gradle,0\5\05efc8b1657769a27696d478ded1e95f38737233
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+Z
+*src/test/java/baseline/Solution32Test.java,b\f\bfd0fd9f1e4a9665e6d03434b09c739570d5dc8b
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+V
+&src/main/java/baseline/Solution32.java,b\d\bd879f0038f65cacac80a75883d242ae15f31cbf
--- exercise33/src/main/java/baseline/Solution33.java
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -5,21 +5,44 @@
 
 package baseline;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Solution33
 {
+    private static final Scanner sc = new Scanner(System.in);
+    private static final Random rand = new Random();
+
+    private static final String[] responses = {"Yes", "No", "Maybe", "Ask again later."};
+
     public static void main(String[] args)
     {
+        Solution33 sol = new Solution33();
+
         // Prompt user for question
+        System.out.println("What's your question?");
+        System.out.print("> ");
+
+        sc.nextLine();
 
         // Generate random number to determine response
+        int randomNumber = sol.getRandomNumber();
 
         // Display chosen response
+        System.out.println("\n" + sol.getResponse(randomNumber));
 
         // Exit
+        System.exit(0);
+    }
+
+    public int getRandomNumber()
+    {
+        return rand.nextInt(responses.length);
     }
 
     public String getResponse(int index)
     {
         // Convert random number to response
+        return responses[index];
     }
 }
