5188ce0e41c0767243481d77694c471e52a55ded
ethandrzb <ethandrzb@gmail.com>
2021-10-02 19:42:20 -0400

    Start of Solution35

---
+++ exercise35/src/main/java/baseline/Solution35.java
@@ -0,0 +1,39 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution 35
+ *  Copyright 2021 Ethan Thomas
+ */
+
+package baseline;
+
+import java.util.ArrayList;
+
+public class Solution35
+{
+    public static void main(String[] args)
+    {
+        // Get names from user
+
+        // Get index of winning name
+
+        // Use random number to select name
+
+        // Display chosen name
+
+        // Exit
+    }
+
+    private ArrayList<String> getNamesFromUser()
+    {
+        // Repeatedly prompt a user to enter a name until the name they enter is blank
+    }
+
+    private int generateRandomIndex()
+    {
+        // Generate a random number to select one of the names entered
+    }
+
+    private String selectWinner(int index)
+    {
+        // Use index to select winning name from list of names
+    }
+}
