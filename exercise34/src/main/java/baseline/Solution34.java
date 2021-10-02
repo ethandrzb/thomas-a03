a32549a489bd81baf1fd476ee9e4624b6683953b
ethandrzb <ethandrzb@gmail.com>
2021-10-02 17:47:01 -0400

    Start of Solution34

---
+++ exercise34/src/main/java/baseline/Solution34.java
@@ -0,0 +1,29 @@
+/*
+ * / UCF COP3330 Fall 2021 Assignment 3 Solution 34
+ *  Copyright 2021 Ethan Thomas
+ */
+
+package baseline;
+
+public class Solution34
+{
+    public static void main(String[] args)
+    {
+        // Display employee list
+
+        // Prompt user for employee to remove
+
+        // Attempt to remove employee
+
+        // Display employee list
+
+        // Exit
+    }
+
+    public void printEmployeeList()
+    {
+        // Display number of employees stored
+
+        // Display all employees stored
+    }
+}
