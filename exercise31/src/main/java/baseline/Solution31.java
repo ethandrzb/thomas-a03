c992a14c8cf27b7f10095284c82c2b02cfd71093
ethandrzb <ethandrzb@gmail.com>
2021-10-01 20:20:59 -0400

    Start of Solution31

---
+++ exercise31/src/main/java/baseline/Solution31.java
@@ -0,0 +1,38 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution 31
+ *  Copyright 2021 Ethan Thomas
+ */
+
+package baseline;
+
+public class Solution31
+{
+    public static void main(String[] args)
+    {
+        // Get resting pulse from user
+
+        // Get age from user
+
+        // Generate target heart rate table
+
+        // Display table
+
+        // Exit
+    }
+
+    public String generateTargetHeartRateTable(int age, int restingHeartRate)
+    {
+        // Generate table header
+
+        // For each intensity i from 55% to 95%
+            // Compute the target heart rate
+            // Append intensity and target heart rate to table
+
+        // Return table
+    }
+
+    public int getTargetHeartRate(int age, int restingHeartRate, double intensity)
+    {
+        // Compute target heart rate for given age, resting heart rate, and intensity
+    }
+}
