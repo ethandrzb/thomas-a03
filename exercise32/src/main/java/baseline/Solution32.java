dc7571bd394954a38a0dbbaff2a171aea1ed7ee8
ethandrzb <ethandrzb@gmail.com>
2021-10-01 21:50:35 -0400

    Start of Solution32

---
+++ exercise32/src/main/java/baseline/Solution32.java
@@ -0,0 +1,48 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution 32
+ *  Copyright 2021 Ethan Thomas
+ */
+
+package baseline;
+
+public class Solution32
+{
+    public static void main(String[] args)
+    {
+        // Display title
+
+        // Run game
+
+        // Exit
+    }
+
+    private void gameRunner()
+    {
+        // Run game
+
+        // Ask user if they want to play again
+            // If so, run game
+            // If not, return
+    }
+
+    private void guessTheNumber()
+    {
+        // Prompt user for difficulty level
+
+        // Get target number for user to guess
+
+        // Prompt user for first guess
+
+        // while user's guess is incorrect
+            // Display whether the user's guess was too high or too low
+            // Prompt user for their guess
+            // Increment number of guesses made so far
+
+        // Display game results
+    }
+
+    private int generateTargetNumber(int difficultyLevel)
+    {
+        // Randomly generate target number within range set by difficulty
+    }
+}
