ca8be7dc8c553e76521393df93f6ebc6134be4ff
ethandrzb <ethandrzb@gmail.com>
2021-10-02 17:13:47 -0400

    Start of Solution33

---
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -0,0 +1,25 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution 33
+ *  Copyright 2021 Ethan Thomas
+ */
+
+package baseline;
+
+public class Solution33
+{
+    public static void main(String[] args)
+    {
+        // Prompt user for question
+
+        // Generate random number to determine response
+
+        // Display chosen response
+
+        // Exit
+    }
+
+    public String getResponse(int index)
+    {
+        // Convert random number to response
+    }
+}
