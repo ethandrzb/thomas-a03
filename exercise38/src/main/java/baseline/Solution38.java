0f6bd05f348653b2a793a2491fb1fbb9511f6548
ethandrzb <ethandrzb@gmail.com>
2021-10-03 19:30:53 -0400

    Start of Solution38

---
+++ exercise38/src/main/java/baseline/Solution38.java
@@ -0,0 +1,32 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution 38
+ *  Copyright 2021 Ethan Thomas
+ */
+
+package baseline;
+
+public class Solution38
+{
+    public static void main(String[] args)
+    {
+        // Prompt user for list of numbers
+
+        // Convert user input to array
+
+        // Generate copy of input array with only even numbers
+
+        // Display even-only array
+
+        // Exit
+    }
+
+    public int[] filterEvenNumbers(int[] array)
+    {
+        // For each element in original array
+            // Check if the element is even
+                // If so, add it to the output array
+                // If not, skip it
+
+        // Return output array
+    }
+}
