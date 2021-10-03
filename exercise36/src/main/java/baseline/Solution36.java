da1119d24cdb7fbb9b3d7c4454927fa901c20ad5
ethandrzb <ethandrzb@gmail.com>
2021-10-02 20:52:04 -0400

    Start of Solution36

---
+++ exercise36/src/main/java/baseline/Solution36.java
@@ -0,0 +1,55 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution 36
+ *  Copyright 2021 Ethan Thomas
+ */
+
+package baseline;
+
+public class Solution36
+{
+    public static void main(String[] args)
+    {
+        // Get data from user
+
+        // Display entered data
+
+        // Compute desired statistics on data
+
+        // Display statistics
+
+        // Exit
+    }
+
+    private void getDataFromUser()
+    {
+        // Repeatedly prompt user for data until "done" is entered
+    }
+
+    private void computeAllStatistics()
+    {
+        // Get average
+        // Get minimum
+        // Get maximum
+        // Get standard deviation
+    }
+
+    public void computeAverage()
+    {
+
+    }
+
+    public void computeMinimum()
+    {
+
+    }
+
+    public void computeMaximum()
+    {
+
+    }
+
+    public void computeStandardDeviation()
+    {
+
+    }
+}
