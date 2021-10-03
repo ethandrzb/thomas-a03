66551d273636b88d88238a5894985672bb3c9e7f
ethandrzb <ethandrzb@gmail.com>
2021-10-03 19:57:58 -0400

    Finished Solution38

--- exercise38/src/main/java/baseline/Solution38.java
+++ exercise38/src/main/java/baseline/Solution38.java
@@ -5,28 +5,85 @@
 
 package baseline;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Solution38
 {
+    private static final Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args)
     {
-        // Prompt user for list of numbers
+        Solution38 sol = new Solution38();
 
-        // Convert user input to array
+        Integer[] numbers;
+
+        boolean validInput = true;
+        do
+        {
+            // Prompt user for list of numbers
+            System.out.print("Enter a list of numbers, separated by spaces: ");
+
+            // Convert user input to array
+            String[] userInput = sc.nextLine().split(" ");
+            numbers = new Integer[userInput.length];
+            for (int i = 0; i < userInput.length; i++)
+            {
+                try
+                {
+                    numbers[i] = Integer.parseInt(userInput[i]);
+
+                } catch (NumberFormatException e)
+                {
+                    System.out.println("Input contains invalid number.");
+                    validInput = false;
+                }
+            }
+        } while (!validInput);
 
         // Generate copy of input array with only even numbers
+        Integer[] evensOnly = sol.filterEvenNumbers(numbers);
 
         // Display even-only array
+        System.out.println("The even numbers are " + sol.arrayToString(evensOnly) + ".");
 
         // Exit
+        System.exit(0);
     }
 
-    public int[] filterEvenNumbers(int[] array)
+    public Integer[] filterEvenNumbers(Integer[] array)
     {
+        ArrayList<Integer> tmp = new ArrayList<>();
+
         // For each element in original array
+        for(int i : array)
+        {
             // Check if the element is even
+            if(i % 2 == 0)
+            {
                 // If so, add it to the output array
-                // If not, skip it
+                tmp.add(i);
+            }
+        }
 
         // Return output array
+        return tmp.toArray(new Integer[0]);
+    }
+
+    private String arrayToString(Integer[] array)
+    {
+        StringBuilder output = new StringBuilder();
+
+        for(int i = 0; i < array.length; i++)
+        {
+            output.append(array[i]);
+
+            if(i < array.length - 1)
+            {
+                output.append(' ');
+            }
+        }
+
+        return output.toString();
     }
 }
