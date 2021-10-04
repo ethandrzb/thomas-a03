cbdebddceec8f4e567941e4b3c9480c7285575bd
ethandrzb <ethandrzb@gmail.com>
2021-10-03 22:32:10 -0400

    Start of Solution40

---
+++ exercise40/src/main/java/baseline/Solution40.java
@@ -0,0 +1,59 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution 40
+ *  Copyright 2021 Ethan Thomas
+ */
+
+package baseline;
+
+public class Solution40
+{
+    public static void main(String[] args)
+    {
+        // Generate employee database
+
+        // Prompt user for search query
+
+        // Generate sub-database of entries related to search query
+
+        // Display search results
+
+        // Exit
+    }
+
+    public void generateEmployeeDatabase()
+    {
+        // Create map for first employee
+
+        // Add employee's first name to map
+
+        // Add employee's last name to map
+
+        // Add employee's position to map
+
+        // Add employee's separation date to map
+
+        // Add employee map to list
+
+        // Repeat for remaining employees
+    }
+
+    public String displayDatabase()
+    {
+        // Initialize columns
+
+        // Add column headers
+
+        // Add data to each column
+
+        // Get widths of each column
+
+        // Insert calculated column widths into row format string
+
+        // Generate header
+
+        // Generate divider
+
+        // Generate remaining rows
+
+    }
+}
