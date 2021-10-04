bd3a27104b2033f66c29b366a296129d26be33c3
ethandrzb <ethandrzb@gmail.com>
2021-10-03 20:17:34 -0400

    Start of Solution39

---
+++ exercise39/src/main/java/baseline/Solution39.java
@@ -0,0 +1,42 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution 39
+ *  Copyright 2021 Ethan Thomas
+ */
+
+package baseline;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class Solution39
+{
+    public static void main(String[] args)
+    {
+        // Generate employee database
+
+        // Sort database
+
+        // Display database
+
+        // Exit
+    }
+
+    public List<HashMap<String, String>> generateEmployeeDatabase()
+    {
+        // Create map for first employee
+
+        // Add employee's first name to map
+
+        // Add employee's last name to map
+
+        // Add employee's position to map
+
+        // Add employee's separation date to map
+
+        // Add employee map to list
+
+        // Repeat for all employees
+
+        // Return list of employee maps
+    }
+}
